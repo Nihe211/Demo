8ad503fe95b72f21177617e32dc9c6c8fc33d252
Nihe211 <thanhhaoao213@gmai.com>
2024-05-16 20:45:06 +0700

    Doi output

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -15,7 +15,7 @@ public class TestGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("123");
+        System.out.println("1234");
     }
     
 }
